8dd8907d660d82e68d92f9517d729613357b9311
Zulfikar Muhamad <zul.m@icloud.com>
2023-07-10 15:12:25 +0800

    Added module to maps to the next.js page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    exportTrailingSlash: true,
+    exportPathMap: function() {
+        return {
+            '/': { page: '/' }
+        };
+    }
+};
